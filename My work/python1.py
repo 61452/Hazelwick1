afda0056151e9eb39546692684916be72aa1f937
61452 <24BACCIARELLICh@hazelwick.org>
2026-09-18 13:57:49 +0000

    09/18/26

---
+++ My work/python1.py
@@ -0,0 +1,20 @@
+n1=input("Enter the name of the first pig: ")
+n2=input("Enter the name of the second pig: ")
+n3=input("Enter the name of the third pig: ")
+m1=input("Enter the material of the first house: ")
+m2=input("Enter the material of the second house: ")
+m3=input("Enter the material of the third house: ")
+n4=input("Enter the name of the big bad wolf: ")
+
+
+
+
+print(f"""There once were three little pigs. The first
+pig was called {n1}, the second was called {n2}
+and the third was called {n3}. The first house
+was made from {m1}. The second house was made
+from {m2}. The third house was made from {m3}.
+The big bad wolf was called {n4}. The big bad wolf
+blew down the first two houses. He could not
+destroy the third house made from bricks.
+The pigs lived happily ever after.""")
